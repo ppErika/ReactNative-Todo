9044b4c0c6378dadeecfc6414adfd935c9034c16
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-06 11:55:01 +0900

    Input 스타일 수정

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function App() {
           backgroundColor={theme.background}
         />
         <Title>TODO List</Title>
-        <Input />
+        <Input placeholder="+ Add a Task" />
       </Container>
     </ThemeProvider>
   );
--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,9 @@ import styled from 'styled-components/native';
 import {Dimensions, useWindowDimensions} from 'react-native';
 
 // 양쪽에 20px씩 남기고 띄워주기 위해 width 크기를 조절
-const StyledInput = styled.TextInput`
+const StyledInput = styled.TextInput.attrs(({theme}) => ({
+  placeholderTextColor: theme.main,
+}))`
   width: ${({width}) => width - 40}px;
   height: 60px;
   margin: 3px 0;
@@ -14,11 +16,22 @@ const StyledInput = styled.TextInput`
   color: ${({theme}) => theme.text};
 `;
 
-const Input = () => {
+const Input = ({placeholder}) => {
   // 사용하는 기기의 화면의 크기(width)를 가져옴
   //   const width = Dimensions.get('window').width;
   const width = useWindowDimensions().width;
-  return <StyledInput width={width} />;
+  return (
+    <StyledInput
+      width={width}
+      placeholder={placeholder}
+      maxLength={50}
+      //  대문자로 시작 안하기
+      autoCapitalize="none"
+      //  밑에 두개는 아이폰 전용인듯 (오타안잡아주기, return키를 done으로 보여주기)
+      autoCorrect={false}
+      returnKeyType="done"
+    />
+  );
 };
 
 export default Input;
