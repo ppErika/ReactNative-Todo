14f2792872a479c7b18ce2ee41a5f1cbffffa945
LAPTOP-KHDAKGR7\erika <hanerika11@gmail.com>
2021-08-06 11:41:48 +0900

    같은 기능을 다른 방식으로 사용하기 useWindowDimensions

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import {Dimensions} from 'react-native';
+import {Dimensions, useWindowDimensions} from 'react-native';
 
+// 양쪽에 20px씩 남기고 띄워주기 위해 width 크기를 조절
 const StyledInput = styled.TextInput`
   width: ${({width}) => width - 40}px;
   height: 60px;
@@ -14,7 +15,9 @@ const StyledInput = styled.TextInput`
 `;
 
 const Input = () => {
-  const width = Dimensions.get('window').width;
+  // 사용하는 기기의 화면의 크기(width)를 가져옴
+  //   const width = Dimensions.get('window').width;
+  const width = useWindowDimensions().width;
   return <StyledInput width={width} />;
 };
 
